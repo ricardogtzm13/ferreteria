3ba40c0426470a981ef3019644cba2c859fd754e
Axel Morales <axel@moralestorres.dev>
2022-11-23 20:17:29 -0600

    código wonito

--- functions.py
+++ functions.py
@@ -4,15 +4,14 @@ from gui import gui
 import matplotlib.pyplot as plt
 def fn1():
 	global db
-	indice=0
-	dic={}
+	indice = 0
+	dic = {}
 	moda = db.execute("SELECT * FROM productopedido")['CodigoProducto'].mode().tolist()[0]
 	producto = db.execute("SELECT YEAR(pedido.Fecha) AS año, COUNT(pedido.Codigo) AS cuenta FROM productopedido JOIN pedido ON productopedido.CodigoPedido = pedido.Codigo WHERE productopedido.CodigoProducto = {} GROUP BY YEAR(pedido.Fecha)".format(moda)).to_dict()
 	for i in producto["año"].keys():
-		indice +=  1
+		indice += 1
 		dic[producto["año"][i]]=producto["cuenta"][i]
 		gui.listbox.insert(indice, "%s --- (%s)" % (producto["año"][i], producto["cuenta"][i]))
-
 	plt.bar(dic.keys(), dic.values())
 	plt.xlabel("Año")
 	plt.ylabel("Ventas")
@@ -24,7 +23,7 @@ def fn2():
 	dic = {}
 	montos = db.execute("SELECT MONTHNAME(Fecha) AS mes, SUM(Monto) AS suma FROM pedido WHERE Fecha BETWEEN '2022-08-01' AND '2022-11-31'")
 	for i in montos["mes"].keys():
-		indice +=  1
+		indice += 1
 		dic[montos["mes"][i]] = montos["suma"][i]
 		gui.listbox.insert(indice, "%s --- (%s)" % (montos["mes"][i], montos["suma"][i]))
 	plt.bar(dic.keys(), dic.values())
@@ -32,7 +31,6 @@ def fn2():
 	plt.ylabel("Monto")
 	plt.title("Gráfica Montos")
 	plt.show()
-
 def fn3():
 	global db, gui
 	moda = db.execute("SELECT * FROM productopedido")['CodigoProducto'].mode().tolist()[0]
@@ -41,16 +39,16 @@ def fn3():
 		gui.listbox.insert(i, clientes[i])
 def fn4():
 	global db
-	indice=0
-	dic={}
+	indice = 0
+	dic = {}
 	empleados = db.execute("SELECT empleado.Nombre AS nombreEmpleado, COUNT(pedido.CodigoEmpleado) AS cuenta FROM pedido JOIN empleado ON pedido.CodigoEmpleado = empleado.Codigo GROUP BY CodigoEmpleado LIMIT 5").to_dict()
 	for i in empleados["nombreEmpleado"].keys():
-		indice +=  1
-		dic[empleados["nombreEmpleado"][i]]=empleados["cuenta"][i]
+		indice += 1
+		dic[empleados["nombreEmpleado"][i]] = empleados["cuenta"][i]
 		gui.listbox.insert(indice, "%s --- (%s)" % (empleados["nombreEmpleado"][i], empleados["cuenta"][i]))
-
 	plt.bar(dic.keys(), dic.values())
 	plt.xlabel("Empleado")
+	plt.xticks(rotation=20)
 	plt.ylabel("Ventas")
 	plt.title("Gráfica Empleados")
 	plt.show()
